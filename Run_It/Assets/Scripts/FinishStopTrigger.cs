eef01abb9c2e4fd5188cf344d4e21ee7a307b4ab
franciscc <franciscoviola22@gmail.com>
2021-03-26 23:10:34 -0300

    Added timer and triggers on start and finish points

---
+++ Run_It/Assets/Scripts/FinishStopTrigger.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinishStopTrigger : MonoBehaviour
+{
+    public bool isStartTrigger;
+    public bool isFinishTrigger;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (isFinishTrigger)
+        {
+            GameObject.Find("Player").SendMessage("Finnish");
+        }
+        else
+        {
+            GameObject.Find("Player").SendMessage("StartTimer");
+        }
+    }
+}
---
+++ Run_It/Assets/Scripts/Timer.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    public Text timerText;
+    private float startTime;
+    private bool finnished = false;
+    private bool started = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timerText.color = Color.red;
+        timerText.text = "00:00";    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (finnished) return;
+
+        if(started)
+        {
+            float t = Time.time - startTime;
+            string minutes = ((int)t / 60).ToString("00");
+            string seconds = (t % 60).ToString("00");
+
+            timerText.text = minutes + ":" + seconds;
+        }
+    }
+
+    public void Finnish()
+    {
+        finnished = true;
+        timerText.color = Color.green;
+    }
+    public void StartTimer()
+    {
+        startTime = Time.time;
+        timerText.color = Color.white;
+        started = true;
+    }
+
+}
