4b28691f0f2b6675456341027ecea3a541ae4538
franciscc <franciscoviola22@gmail.com>
2021-04-03 21:16:24 -0300

    Added Pause Menu. Beta version builded and uploaded to https://drive.google.com/file/d/1JC_JyvStdfXL7qcjDR37VeJtk0FD-wza/view?usp=sharing

---
+++ Run_It/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    [SerializeField] private GameObject pauseMenuUI;
+    private bool isPaused;
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            // stop the game
+            if (isPaused)
+            {
+                ResumeGame();
+            }
+            else
+            {
+                PauseGame();
+            }
+        }
+    }
+
+    // Pause Game
+    public void PauseGame()
+    {
+        Time.timeScale = 0;
+
+        // pause the audio
+        AudioListener.pause = true;
+
+        // pause UI
+        pauseMenuUI.SetActive(!isPaused);
+
+        isPaused = true;
+    }
+
+    // Resume Game
+    public void ResumeGame()
+    {
+        Time.timeScale = 1;
+        
+        // pause the audio
+        AudioListener.pause = false;
+
+        // pause UI
+        pauseMenuUI.SetActive(!isPaused);
+
+        isPaused = false;   
+    }
+
+    // Exit App
+    public void ExitApp()
+    {
+        Application.Quit();
+    }
+
+    // Restart Game
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        ResumeGame();
+    }
+
+}
